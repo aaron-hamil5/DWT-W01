28a100299a705b402b1e8d3b242caa023ba5e146
Aaron Hamilton (MacBook Air) <aaronleon28@gmail.com>
2026-09-15 10:27:56 +0100

    Add user input and full name display functionality

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,1 +1,9 @@
 console.log("Welcome to the first lab of Dynamic Web Technologies");
+
+// Create a simple script that uses variables to store a user's first name and last name. Then, use string
+// concatenation or template literals to combine these variables into a full name and print it to the console
+
+const firstName = prompt("What is your first name?");
+const lastName = prompt("What is your last name?");
+const fullName = firstName + " " + lastName;
+console.log(fullName);
