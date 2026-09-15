99c09dcf4da82b97d809dea8de90a0b6a7bc654c
Aaron Hamilton (MacBook Air) <aaronleon28@gmail.com>
2026-09-15 11:04:34 +0100

    Add `student` object with properties and method to calculate average grade in example.js

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,1 +1,23 @@
 console.log("Welcome to the first lab of Dynamic Web Technologies");
+
+// Create an object student with properties name, course, and an array of grades. Add a method to calculate
+// and return the average grade. Tip: Look for the JS arrays in the resources section
+
+// https://www.w3schools.com/js/js_arrays.asp
+// https://stackoverflow.com/questions/60573801/adding-together-any-number-of-items-in-an-array-with-javascript
+
+const student = {
+    name: "John Doh",
+    course: "Mobile and Web Development",
+    grades: [85, 90, 92, 88, 95],
+
+    displayAverage: function() {
+        let gradeTotal = this.grades.reduce((acc, curr) => parseInt(curr) + parseInt(acc)) //Should be: 450
+
+        let avgTotal = gradeTotal / this.grades.length; // Should be: 450 / 5
+
+        console.log(`Average Grade: ${avgTotal}`); // Should be: 90
+    }
+}
+
+student.displayAverage();
